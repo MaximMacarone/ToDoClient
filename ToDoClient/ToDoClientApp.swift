678dd24dfbbca835001542c342759ff66c5b3468
MaximMacarone <maximmacarone@gmail.com>
2024-12-09 13:58:50 +0300

    Initial Commit

---
+++ ToDoClient/ToDoClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoClientApp.swift
+//  ToDoClient
+//
+//  Created by Maxim Makarenkov on 09.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
